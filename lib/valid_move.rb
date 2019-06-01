b665d516b494b6cf217e4fb4121181ddbd28f540
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-01 18:41:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,12 @@
 # code your #valid_move? method here
 def valid_move?()
-end 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, pos)
   if(board[pos]=="" || board[pos]==" " || board[pos]==nil)
     return false
-  else 
+  else
     return true
   end
 end
